d1beeb0772e557df4d950c316a21590ee2362a29
Cliff Jurgensen <jivefox.002@gmail.com>
2018-09-12 03:27:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.;
 
 var theFunk = funkyFunction() {
-  return = function();
+  var anything = function() {
 }
-
+}
 
 
 
